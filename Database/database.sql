f6231e9171388e0bf2d0997bee6ab2d1b9b498c1
Erik Bauer <github@erik-bauer.de>
2020-01-31 20:53:09 +0100

    Added Database Structure

---
+++ Database/database.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host:
+-- Erstellungszeit: 31. Jan 2020 um 19:51
+-- Server-Version: 5.5.47-0+deb6u1
+-- PHP-Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: ``
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `temperaturen`
+--
+
+CREATE TABLE `temperaturen` (
+  `kunde` varchar(255) NOT NULL,
+  `raum` varchar(255) NOT NULL,
+  `temp` float NOT NULL,
+  `feuchtigkeit` float NOT NULL,
+  `zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `temperaturen`
+--
+ALTER TABLE `temperaturen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `temperaturen`
+--
+ALTER TABLE `temperaturen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
